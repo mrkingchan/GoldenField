523fc0f46b496c6b1a9491b6b60b9ddf9b43c1b1
mrkingchan <1203710130@qq.com>
2018-05-04 09:13:35 +0800

    Initial Commit

---
+++ GoldenField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoldenField
+//
+//  Created by Macx on 2018/5/4.
+//  Copyright © 2018年 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
